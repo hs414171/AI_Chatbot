917275b8c020ad13c7be01bbd93143f09f57b60b
Harshit Sharma <hs414171@gmail.com>
2024-09-30 17:33:00 +0530

    changes to the prompt

--- chat_module.py
+++ chat_module.py
@@ -12,6 +12,23 @@ file = open("resumeContext.txt")
 resumeContext = file.read()
 file.close()
 
+introduction = """
+Hello I am Assist GPT a chatbot made by Mr. Harshit Sharma to show his Skills of developing Artificially Intelligent Apps I have all the Information about
+his works his skills all his projects and can also help you contact him.
+
+You could ask me anything For example:
+- "Tell me about Mr. Harshit Sharma"
+- "What are the skills of Mr. Harshit Sharma"
+- "What are the projects of Mr. Harshit Sharma"
+- "How can I contact Mr. Harshit Sharma"
+
+With this i could also help you with anything you want. I am powered by the latest llama Large Language Model.
+
+Nice to meet you.
+
+
+"""
+
 # Initialize Groq LLM
 llm = ChatGroq(temperature=0, model_name="llama-3.2-90b-text-preview")  # Specify the correct model
 
@@ -27,6 +44,12 @@ def handle_conversation(user_input: str, context: str):
 
     With this keep in mind about the context of the chat history to
     further continue the chat session.
+
+    Whenever someone starts to chat with you give an Introduction as follows:
+    {introduction}
+
+    Once introduction is given shouldnt be repeated unless someone asks what
+    you could do then read from {introduction}
 
     This is the resume context = {resumeContext}
     This is the Chat History = {context}
